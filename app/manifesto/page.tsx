ff478c432e4e422fb01da9515e142d1caa6fcc25
Oliver Kiran Brown <oliverkiranbrown@Olivers-MacBook-Pro.local>
2025-11-30 13:31:14 +0000

    silly shell script to pixelate vids, added to manifesto

--- app/manifesto/page.tsx
+++ app/manifesto/page.tsx
@@ -10,6 +10,30 @@ export default function ManifestoPage() {
             <p>
                 It's alive in the sounds of Balloon Tomb...
             </p>
+            <br/>
+            {/* Row of GIFs */}
+            <div className="flex flex-wrap gap-6 justify-start">
+                {/* Single GIF + Title */}
+                <div className="flex flex-col items-center">
+                <span className="font-semibold mb-2">Damian</span>
+                <img src="/wanted/damian_rotated_pixelated.gif" alt="Damian" className="w-50 h-full object-contain"/>
+                </div>
+
+                <div className="flex flex-col items-center">
+                <span className="font-semibold mb-2">Marcus</span>
+                <img src="/wanted/marcus_rotated_pixelated.gif" alt="Marcus" className="w-50 h-full object-contain"/>
+                </div>
+
+                <div className="flex flex-col items-center">
+                <span className="font-semibold mb-2">Oli</span>
+                <img src="/wanted/oli_rotated_pixelated.gif" alt="Oli" className="w-50 h-full object-contain"/>
+                </div>
+
+                <div className="flex flex-col items-center">
+                <span className="font-semibold mb-2">Taylor</span>
+                <img src="/wanted/taylor_rotated_pixelated.gif" alt="Taylor" className="w-50 h-full object-contain"/>
+                </div>
+            </div>
 
         </div>
     );
