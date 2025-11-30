146bca41e533c6de20db07b592cdd2e1e8419af0
Oliver Kiran Brown <oliverkiranbrown@Olivers-MacBook-Pro.local>
2025-11-30 15:35:04 +0000

    added popup diaglogue for new gifs

--- app/manifesto/page.tsx
+++ app/manifesto/page.tsx
@@ -1,3 +1,6 @@
+import { Dialog, DialogContent, DialogTrigger, DialogTitle, DialogDescription } from "@/components/ui/pixelact-ui/dialog";
+import { Button } from "@/components/ui/pixelact-ui/button";
+
 export default function ManifestoPage() {
     return (
         <div className="p-6 bg-white text-black min-h-screen flex flex-col">
@@ -12,29 +15,80 @@ export default function ManifestoPage() {
             </p>
             <br/>
             {/* Row of GIFs */}
-            <div className="flex flex-wrap gap-6 justify-start">
-                {/* Single GIF + Title */}
+            <div className="flex flex-wrap gap-6 justify-center d">
                 <div className="flex flex-col items-center">
-                <span className="font-semibold mb-2">Damian</span>
+                <Dialog>
+                    <DialogTrigger asChild>
+                        <span className="font-semibold mb-2 hover:text-pink-500 hover:bg-yellow-200 cursor-pointer">Damian</span>
+                    </DialogTrigger>
+                    <DialogContent>
+                        <DialogTitle>This is Damian</DialogTitle>
+                        <DialogDescription>
+                            He plays guitar.
+                        </DialogDescription>
+                    </DialogContent>
+                </Dialog>
                 <img src="/wanted/damian_rotated_pixelated.gif" alt="Damian" className="w-50 h-full object-contain"/>
                 </div>
 
                 <div className="flex flex-col items-center">
-                <span className="font-semibold mb-2">Marcus</span>
+                
+                <Dialog>
+                    <DialogTrigger asChild>
+                        <span className="font-semibold mb-2 hover:text-pink-500 hover:bg-yellow-200 cursor-pointer">Marcus</span>
+                    </DialogTrigger>
+                    <DialogContent>
+                        <DialogTitle>This is Marcus</DialogTitle>
+                        <DialogDescription>
+                            He shouts.
+                        </DialogDescription>
+                    </DialogContent>
+                </Dialog>
                 <img src="/wanted/marcus_rotated_pixelated.gif" alt="Marcus" className="w-50 h-full object-contain"/>
                 </div>
-
+            </div>
+            <br/>
+            <div className="flex flex-wrap gap-6 justify-center">
                 <div className="flex flex-col items-center">
-                <span className="font-semibold mb-2">Oli</span>
-                <img src="/wanted/oli_rotated_pixelated.gif" alt="Oli" className="w-50 h-full object-contain"/>
+                    <Dialog>
+                        <DialogTrigger asChild>
+                            <span className="font-semibold mb-2 hover:text-pink-500 hover:bg-yellow-200 cursor-pointer">Oli</span>
+                        </DialogTrigger>
+                        <DialogContent>
+                            <DialogTitle>This is Oli</DialogTitle>
+                            <DialogDescription>
+                                He plays drums.
+                            </DialogDescription>
+                        </DialogContent>
+                    </Dialog>
+                    <img
+                    src="/wanted/oli_rotated_pixelated.gif"
+                    alt="Oli"
+                    className="w-50 h-full object-contain border-4 border-gray-300 hover:border-pink-500 transition-colors duration-300"
+                />
                 </div>
 
                 <div className="flex flex-col items-center">
-                <span className="font-semibold mb-2">Taylor</span>
-                <img src="/wanted/taylor_rotated_pixelated.gif" alt="Taylor" className="w-50 h-full object-contain"/>
+                
+                <Dialog>
+                    <DialogTrigger asChild>
+                        <span className="font-semibold mb-2 hover:text-pink-500 hover:bg-yellow-200 cursor-pointer">Taylor</span>
+                    </DialogTrigger>
+                    <DialogContent>
+                        <DialogTitle>This is Taylor</DialogTitle>
+                        <DialogDescription>
+                            He plays bass.
+                        </DialogDescription>
+                    </DialogContent>
+                </Dialog>
+                <img
+                    src="/wanted/taylor_rotated_pixelated.gif"
+                    alt="Taylor"
+                    className="w-50 h-full object-contain border-4 border-gray-300 hover:border-pink-500 transition-colors duration-300"
+                />
                 </div>
             </div>
-
         </div>
+        
     );
 }
