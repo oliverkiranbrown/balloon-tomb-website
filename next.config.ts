5ea0f293e5587ed80b44c58f2924260d67b0506f
oliverkiranbrown <oliverkiranbrown@gmail.com>
2025-12-16 20:52:49 +0000

    NavBar vs navbar issue causing havoc on linux

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  
+  // catch case sensitivity issues before deployment
+  reactStrictMode: true,
 };
 
 export default nextConfig;
+  
